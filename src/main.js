c12e666b40d6856aecbd9f1de5d3652335c80d8e
yilen3 <1413599820@qq.com>
2022-04-07 22:55:52 +0800

    test

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import store from './store'
 // 引入 Element
 import ElementUI from 'element-ui'
 // 引入 Element 的主题文件
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义的全局样式
+import './styles/index.scss'
 // 将 Element 注册为 Vue 插件
 Vue.use(ElementUI)
 
