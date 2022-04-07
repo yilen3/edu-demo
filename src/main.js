dac80de7b2220eec73c5b751f66624e0193c0254
yilen3 <1413599820@qq.com>
2022-04-07 22:33:41 +0800

    master

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 Element
+import ElementUI from 'element-ui'
+// 引入 Element 的主题文件
+import 'element-ui/lib/theme-chalk/index.css'
+// 将 Element 注册为 Vue 插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
